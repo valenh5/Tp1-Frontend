bd002c1fe7e77c5e54ba1aeb5fd0d4b610ab9121
valenh5 <alumno25.huerga.valentin@ipm.edu.ar>
2025-04-18 17:04:14 -0300

    Arreglo de errores, ya aparece en angular

---
+++ src/app/telefonos/telefonos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-telefonos',
+  imports: [],
+  templateUrl: './telefonos.component.html',
+  styleUrl: './telefonos.component.css'
+})
+export class TelefonosComponent {
+
+}
